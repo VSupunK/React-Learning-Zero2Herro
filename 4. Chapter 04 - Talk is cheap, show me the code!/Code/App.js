a6cb9699cf9ac893c705381e135055508463ca9e
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-09-17 19:24:04 +0530

    Create a Food Card

--- 4. Chapter 04 - Talk is cheap, show me the code!/Code/App.js
+++ 4. Chapter 04 - Talk is cheap, show me the code!/Code/App.js
@@ -1,6 +1,16 @@
 import React from "react";
 import ReactDOM from "react-dom/client";
 
+//If we need to add styles in here, we can do it as JS object
+const styleCard = {
+    padding: "5px",
+    width: "200px",
+    height: "300px",
+    padding: "10px",
+    border: "1px solid black",
+    backgroundColor: "yellow"
+}
+
 //header
 const Header = () => {
     return (
@@ -22,8 +32,13 @@ const Header = () => {
 
 const RestaurantCard = () => {
     return (
-<div className="res-card">
+<div className="res-card" style={styleCard}>
+    <img className="res-logo" src="https://www.southernliving.com/thmb/j_6gABRIAMegN6RFHxOgbUqBxjA=/750x0/filters:no_upscale():max_bytes(150000):strip_icc():format(webp)/2652401_QFSSL_SupremePizza_00072-d910a935ba7d448e8c7545a963ed7101.jpg" alt="pizza-img"/>
+    
         <h3>Pizza Hut</h3>
+        <h4>Itali</h4>
+        <h4>4.4 Stars</h4>
+        <h4>26 minutes</h4>
     </div>
     )
     
