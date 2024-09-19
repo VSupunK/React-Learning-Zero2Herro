c126d79e89f9d7c0a80d226374dc27825948a1e3
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-09-19 19:29:49 +0530

    Make a restFilter using useState

--- 5. Chapter 05 - Let's get Hooked/Code/src/components/Body.js
+++ 5. Chapter 05 - Let's get Hooked/Code/src/components/Body.js
@@ -1,28 +1,31 @@
 import RestaurantCard from "./RestaurantCard";
 import resList from "../utils/mockData";
+import { useState } from 'react';
 
 const Body = () => {
+
+    //Local State Variable - useState() -> this is a react hook
+    const [listOfRestaurants, setListOfRestaurants] = useState(resList);
+
     return (
         <div className="body">
-            <div className="Search">Search
-            </div>
-                <div className="restaurant-container"> 
-                    {/* RestaurantCard */}
-                    {/* <RestaurantCard resData = {resList[0]}/>
-                    <RestaurantCard resData = {resList[1]}/>
-                    <RestaurantCard resData = {resList[2]}/>
-                    <RestaurantCard resData = {resList[3]}/>
-                    <RestaurantCard resData = {resList[4]}/>
-                    <RestaurantCard resData = {resList[5]}/>
-                    <RestaurantCard resData = {resList[6]}/>
-                    <RestaurantCard resData = {resList[7]}/>
-                    <RestaurantCard resData = {resList[8]}/>
-                    <RestaurantCard resData = {resList[9]}/>
-                    <RestaurantCard resData = {resList[10]}/> */}
+            <div className="filter">
+                <button className="filter-btn" 
+                onClick={() => {
+                    const filteredList = listOfRestaurants.filter(
+                        (res) => res.data.avgRating > 4)
+                        ;
+                        setListOfRestaurants(filteredList);
                     
-
+                }}>
+                    Top Rated Restaurants
+                    </button>
+            </div>
+            {/* <div className="Search">Search
+            </div> */}
+                <div className="restaurant-container"> 
                     {/* When we use .map -> We need to make sure to add a unique key value */}
-                    {resList.map((restaurant) => (
+                    {listOfRestaurants.map((restaurant) => (
                         <RestaurantCard key={restaurant.data.id} resData = {restaurant} />
                     ))}
                 </div>
