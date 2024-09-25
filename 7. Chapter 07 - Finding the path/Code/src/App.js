b2d8fb802ee95eeff72430b1b1cd61325b5a66d1
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-09-25 14:23:05 +0530

    Make children route

--- 7. Chapter 07 - Finding the path/Code/src/App.js
+++ 7. Chapter 07 - Finding the path/Code/src/App.js
@@ -3,9 +3,11 @@ import ReactDOM from "react-dom/client";
 import Header from "./components/Header";
 import Body from "./components/Body";
 import About from "./components/About";
-import { createBrowserRouter, RouterProvider } from "react-router-dom";
 import Contact from "./components/Contact";
 import Error from "./components/Error";
+import Cart from "./components/Cart";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+
 
 
 
@@ -15,7 +17,7 @@ const AppLayout = () => {
     return (
         <div className="app">
             <Header />
-            <Body />
+            <Outlet />
         </div>
     );
 };
@@ -24,16 +26,27 @@ const AppRouter = createBrowserRouter([
     {
         path: '/',
         element: <AppLayout />,
-        errorElement: <Error />
+        children: [
+            {
+                path: '/',
+                element: <Body />,
+            },
+            {
+                path: '/about',
+                element: <About />,
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+            },
+            {
+                path: '/cart',
+                element: <Cart />,
+            }
+        ],
+        errorElement: <Error />,
     },
-    {
-        path: '/about',
-        element: <About />,
-    },
-    {
-        path: '/contact',
-        element: <Contact />,
-    }
+    
 ])
 
 const root = ReactDOM.createRoot(document.getElementById("root"));
--- 7. Chapter 07 - Finding the path/Code/src/components/Header.js
+++ 7. Chapter 07 - Finding the path/Code/src/components/Header.js
@@ -1,5 +1,6 @@
 import { useState } from 'react';
 import { LOGO_URL } from '../utils/constants'
+import { Link } from "react-router-dom";
 
 const Header = () => {
     const [btnNameReact, setBtnNameReact] = useState('Login');
@@ -11,10 +12,10 @@ const Header = () => {
         </div>
         <div className="nav-items">
             <ul>
-                <li>Home</li>
-                <li>About</li>
-                <li>Contact Us</li>
-                <li>Cart</li>
+                <li><Link to='/'>Home</Link></li>
+                <li><Link to='/about'>About</Link></li>
+                <li><Link to='/contact'>Contact Us</Link></li>
+                <li><Link to='/cart'>Cart</Link></li>
                 <button className='loginBtn' onClick={() => {
                     btnNameReact === 'Login' ? setBtnNameReact("Logout") : setBtnNameReact("Login")
                 }}>
