e2ecf384818ff092f51503b96d17658c1434198c
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-09-29 18:16:04 +0530

    Make state bariable inside class component

--- 8. Chapter 08 - Let's grt Classy/Code/src/components/UserClass.js
+++ 8. Chapter 08 - Let's grt Classy/Code/src/components/UserClass.js
@@ -4,13 +4,26 @@ class UserClass extends React.Component {
     constructor(props) {
         super(props)
 
-        console.log(props);
+        this.state = {
+            //This is the object of multiple state variables inside it
+            count1: 0,
+            count2: 2, 
+        }
     }
 
     render() {
         const { name, location } = this.props;
+        const { count1, count2 } = this.state;
         return (
             <div className="user-card">
+                <button onClick={() => {
+                    this.setState({
+                        count1: this.state.count1 + 1
+                    })
+                }
+
+                }>Click to increase the count</button>
+                <h2>Count = {count1}</h2>
                 <h2>Name: {name}</h2>
                 <h3>Location: {location}</h3>
                 <h4>Contact: @info4125</h4>
