dd93ea50960eb833bbdbc6bd49134a1ca30af6da
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-09-29 16:53:34 +0530

    Implement class component and use props

--- 8. Chapter 08 - Let's grt Classy/Code/src/components/About.js
+++ 8. Chapter 08 - Let's grt Classy/Code/src/components/About.js
@@ -1,8 +1,13 @@
+import User from "./User"
+import UserClass from "./UserClass";
+
 const About = () => {
     return (
         <div>
             <h1>About Us</h1>
             <p>Hello everyone</p>
+            <User />
+            <UserClass name={"Supun Kalhara"} location={"Colombo"}/>
         </div>
     )
 }
