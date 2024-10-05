690d40aa9ef379240faa3f5bd6a762c1cd909471
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-05 15:04:42 +0530

    Online/offline status

--- 9. Chapter 09 - Optimising Our App/Code/src/components/Header.js
+++ 9. Chapter 09 - Optimising Our App/Code/src/components/Header.js
@@ -1,9 +1,11 @@
 import { useState } from 'react';
 import { LOGO_URL } from '../utils/constants'
 import { Link } from "react-router-dom";
+import useOnlineStatus from '../utils/useOnlineStatus';
 
 const Header = () => {
     const [btnNameReact, setBtnNameReact] = useState('Login');
+    const onlineStatus = useOnlineStatus();
 
     return (
         <div className="header">
@@ -12,6 +14,7 @@ const Header = () => {
         </div>
         <div className="nav-items">
             <ul>
+                <li>Online Status : {onlineStatus ? "🟢" : "🔴"}</li>
                 <li><Link to='/'>Home</Link></li>
                 <li><Link to='/about'>About</Link></li>
                 <li><Link to='/contact'>Contact Us</Link></li>
