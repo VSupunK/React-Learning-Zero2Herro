40f02caabea1bae7e1fe2427d8ff0e1190df6dc4
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-05 09:53:50 +0530

    Episode 09 - Optimizing our app

---
+++ 9. Chapter 09 - Optimising Our App/Code/src/App.js
@@ -0,0 +1,59 @@
+import React from "react";
+import ReactDOM from "react-dom/client";
+import Header from "./components/Header";
+import Body from "./components/Body";
+import About from "./components/About";
+import Contact from "./components/Contact";
+import Error from "./components/Error";
+import Cart from "./components/Cart";
+import { createBrowserRouter, RouterProvider, Outlet } from "react-router-dom";
+import RestaurantMenu from "./components/RestaurantMenu";
+
+
+
+
+
+
+const AppLayout = () => {
+    return (
+        <div className="app">
+            <Header />
+            <Outlet />
+        </div>
+    );
+};
+
+const AppRouter = createBrowserRouter([
+    {
+        path: '/',
+        element: <AppLayout />,
+        children: [
+            {
+                path: '/',
+                element: <Body />,
+            },
+            {
+                path: '/about',
+                element: <About />,
+            },
+            {
+                path: '/contact',
+                element: <Contact />,
+            },
+            {
+                path: '/cart',
+                element: <Cart />,
+            },
+            {
+                path: '/restaurant/:resId',
+                element: <RestaurantMenu />,
+            }
+        ],
+        errorElement: <Error />,
+    },
+    
+])
+
+const root = ReactDOM.createRoot(document.getElementById("root"));
+
+root.render(<RouterProvider router={AppRouter} />);
---
+++ 9. Chapter 09 - Optimising Our App/Code/src/components/Cart.js
@@ -0,0 +1,9 @@
+const Cart = () => {
+    return (
+        <div>
+            <h1>This is the Cart page</h1>
+        </div>
+    )
+}
+
+export default Cart;
