9cf47cb78207e1846efed34fb3d0eed9d18d6f62
Supun Kalhara <116533030+VSupunK@users.noreply.github.com>
2024-10-16 23:15:38 +0530

    Add styles

--- 11. Chapter 11 - Data is the new oil/Code/src/components/MenuItemList.js
+++ 11. Chapter 11 - Data is the new oil/Code/src/components/MenuItemList.js
@@ -7,11 +7,13 @@ const MenuItemList = (items) => {
     <div>
         {items.items.map((item) => (
         <div key={item.card.info.id} className="p-2 m-2 border-gray-300 border-b-2">
-            <div>
-                <span>{item.card.info.name}</span>
-                <span>{item.card.info.price}<span><FaIndianRupeeSign /></span></span>
+            <div className="flex justify-between pb-5">
+                <div className="font-bold bg-orange-300 py-1 px-4 rounded-full">{item.card.info.name}</div>
+                <div className="flex gap-1 items-center bg-green-400 py-1 px-4 rounded-full"><label className="font-bold">{item.card.info.price ? item.card.info.price / 100 : item.card.info.defaultPrice / 100}</label>
+                <FaIndianRupeeSign />
+                </div>
             </div>
-            <p className="text-xs text-left">{item.card.info.description}</p>
+            <p className="text-md text-left">{item.card.info.description}</p>
         </div>
       ))}
       
